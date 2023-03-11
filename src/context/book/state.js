f54c702ad4f4646c2f1782799ba7fb3b2cc67eaf
Carolina Rossello <c.rossello@ufl.edu>
2023-03-11 01:12:39 -0500

    state js export

--- src/context/book/state.js
+++ src/context/book/state.js
@@ -1,8 +1,16 @@
 // TODO: Load/parse 'favoriteBooks' from localStorage
+const myBooks = localStorage.getItem('favoriteBooks');
+const favoriteBooks = myBooks ? JSON.parse(myBooks) : [];
+
+
 // TODO: Export initial state object as default
 // initial state should have keys bookSearchResults and favoriteBooks
 // bookSearchResults should be an empty array
 // favoriteBooks should be the value from localStorage or an empty array if localStorage value is falsy
-const initialState = {}
 
-export default initialState
+const initialState = {
+    bookSearchResults: [],
+    favoriteBooks: favoriteBooks
+};
+
+export default initialState;
